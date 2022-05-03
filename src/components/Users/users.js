ce85884a6255dcacda4986408d1a60f281842cf5
Andriy2007 <91556804+Andriy2007@users.noreply.github.com>
2022-05-04 00:22:25 +0300

    react

---
+++ src/components/Users/users.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+
+import {User} from "../User/user";
+import {usersService} from "../../service";
+
+
+export const Users = ({ setUser, setUserIdForPosts }) => {
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        usersService.getAll().then(({ data }) => setUsers(data))
+    }, []);
+
+    return (
+        <div>
+            {
+                users
+                    ? users.map((user) => <User key={user.id} user={user} setUser={setUser} setUserIdForPosts={setUserIdForPosts}/>)
+                    : "Loading.."
+            }
+        </div>
+    );
+}
